8a0f64900910d8d518dd954172ed7f41b30e910f
dhcent <huangdan0315@gmail.com>
2026-07-05 22:43:59 -0400

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if nums[0] < nums[-1]:
+            R = 0
+        else:
+            L = 0
+            R = len(nums) - 1
+            # we want to terminate loop when they are adjacent to each other (at inflection point)
+            while R - L > 1:
+                center = int(L + (R - L) / 2)
+                # left part of the list is sorted, inflection pt on right
+                if nums[center] > nums[L]:
+                    L = center
+                else: #center < nums[L], inflection pt on the left
+                    R = center
+        #your shift is by R. so to determine 
+        shift = R
+        # implement binary search: L is 0, R is len - 1. When you access elements, add by shift
+        # then mod the length
+        L = 0
+        R = len(nums) - 1
+        while L <= R:
+            center = L + (R - L) // 2
+            val = nums[(center + shift) % len(nums)]
+            if val == target:
+                return (center + shift) % len(nums)
+            elif val > target:
+                R = center - 1
+            else:
+                L = center + 1
+        return -1
+
